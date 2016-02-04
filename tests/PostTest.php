d5911e301d85d2db58bea64bb80dea5a5321ef37
Ouadie BOUSSAID <ouadie.boussaid@gmail.com>
2016-02-04 07:40:48 +0100

    - adding unit tests to Post api

---
+++ tests/PostTest.php
@@ -0,0 +1,54 @@
+<?php
+
+use Laravel\Lumen\Testing\DatabaseTransactions;
+use Illuminate\Http\Request;
+
+class PostTest extends TestCase
+{
+  /**
+  * Testing the Get all posts
+  *
+  * @return void
+  */
+  public function testGetAllPostsResponseStatus()
+  {
+    $response = $this->call('GET', 'api/posts');
+    $this->assertEquals(200, $response->status());
+  }
+  /**
+  * Testing the Get all posts
+  *
+  * @return void
+  */
+  public function testGetAllPosts()
+  {
+    $this->get('api/posts')
+            ->seeJson([
+               'count' => 200,
+            ]);
+  }
+
+  /**
+  * Testing the Get post by id
+  *
+  * @return void
+  */
+  public function testGetPostById()
+  {
+    $id = 8665268;
+    $this->get("api/posts/$id")
+              ->seeJson(['id' => $id]);
+  }
+
+  /**
+  * Testing the Get post by author
+  *
+  * @return void
+  */
+  public function testGetPostByAuthor()
+  {
+    $author = "maniokmaniok ";
+    $this->get("api/posts?author=$author")
+            ->seeJson(['author' => $author]);
+  }
+}
